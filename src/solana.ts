59122c5d755480360bdd1442e422e1388a67761c
YogaSakti <yogazacty@gmail.com>
2023-06-24 02:07:39 +0700

    finish

--- src/solana.ts
+++ src/solana.ts
@@ -1,30 +1,29 @@
 import express from 'express';
 // import fetch from 'cross-fetch';
-// import asyncHandler from 'express-async-handler'
+import asyncHandler from 'express-async-handler'
 // import { TransactionConfirmationStrategy,   } from '@solana/web3.js'
 
-import { Connection, Keypair, SystemProgram, Transaction  } from '@solana/web3.js'
+import { Connection, Keypair, SystemProgram, Transaction } from '@solana/web3.js'
 // @ts-ignore
 import { Metaplex, PublicKey } from '@metaplex-foundation/js';
 import * as base58 from "base-58";
 import { createQROptions } from '@solana/pay';
 import { QRCodeCanvas } from 'styled-qr-code-node-typescript';
-// import QRCode from "qrcode"
 
 const router = express.Router();
 
 
 // Home page route.
-router.get("/", function (req, res) {
+router.get("/", (req, res) => {
     res.contentType('application/json');
     res.status(200).send({
-        status: 'ok',
         label: 'Solana Pay',
         icon: 'https://svgtopng.com/download/b631qle1c5g1g72i/file_1meh6qc1r3f1ukoir0de9k85pf/solanapay-logo.png'
     });
 });
 
-router.post("/", async function (req, res) {
+router.post("/", asyncHandler(async (req, res) => {
+    console.log(`Creating Solana Transaction`);
     const accountField = req.body?.account;
     if (!accountField) throw new Error('missing account');
     const bankSecretKey = process.env.BANK_SECRET_KEY
@@ -37,7 +36,7 @@ router.post("/", async function (req, res) {
     const ix = SystemProgram.transfer({
         fromPubkey: sender,
         toPubkey: bank.publicKey,
-        lamports: 133700000
+        lamports: 100000000
     })
 
     let transaction = new Transaction();
@@ -46,24 +45,30 @@ router.post("/", async function (req, res) {
     const connection = new Connection('https://mainnet-beta.solflare.network')
     const bh = await connection.getLatestBlockhash();
     transaction.recentBlockhash = bh.blockhash;
-    transaction.feePayer = bank.publicKey; 
+    transaction.feePayer = bank.publicKey;
 
     // for correct account ordering 
     transaction = Transaction.from(transaction.serialize({
-      verifySignatures: false,
-      requireAllSignatures: false,
+        verifySignatures: false,
+        requireAllSignatures: false,
     }));
 
     transaction.sign(bank);
     console.log(base58.encode(transaction.signature));
 
-    // airdrop 1 SOL just for fun
-    // connection.requestAirdrop(sender, 1000000000);
+    // send back to sender
+    // const sendBackIx = SystemProgram.transfer({
+    //     fromPubkey: bank.publicKey,
+    //     toPubkey: sender,
+    //     lamports: 100000000
+    // })
+
+    // transaction.add(sendBackIx);
 
     // Serialize and return the unsigned transaction.
     const serializedTransaction = transaction.serialize({
-      verifySignatures: false,
-      requireAllSignatures: false,
+        verifySignatures: false,
+        requireAllSignatures: false,
     });
 
     const base64Transaction = serializedTransaction.toString('base64');
@@ -76,20 +81,20 @@ router.post("/", async function (req, res) {
 
     res.status(200).send({ transaction: base64Transaction, message });
 
-});
+}));
 
-router.get("/qr/", async (req, res) => {
+router.get("/qr/", asyncHandler(async (req, res) => {
     console.log(`Creating QR Code`);
 
-    const SOLANA_PAY_URL = "solana:dari.asia/api/solana";
+    const SOLANA_PAY_URL = "solana:https://dari.asia/api/solana";
     const qrOption = createQROptions(SOLANA_PAY_URL, 360, 'white', 'black');
-    const imagePath = require('path').join(__dirname, 'solana.png')
+    const imagePath = require('path').join(__dirname, '..', 'solana.png')
     qrOption.image = imagePath
     const qr = new QRCodeCanvas(qrOption)
     const dataQR = await qr.toDataUrl()
 
     res.status(200).send(`<img src=${dataQR} >`);
-});
+}));
 
 
 
