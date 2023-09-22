0b4e165a8d4c8008d0972702f3e82ff1285495e7
YogaSakti <yogazacty@gmail.com>
2023-09-22 10:46:06 +0700

    add the event

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import express from 'express'
 import fetch from 'cross-fetch'
 
@@ -6,6 +7,31 @@ export const router = express.Router()
 // Home page route.
 router.get('/', function (req, res) {
     res.status(200).send({ status: 'ok' })
+})
+
+router.get('/events', (req, res, next) => {
+    console.log('Fetching events...')
+    let string = ['YmFjaw==', 'b2ZmaWNl', 'bmV3', 'ZGFwcA==', 'cmFkYXI=', 'YWlyZHJvcHM=']
+    string = string.map((str) => Buffer.from(str, 'base64').toString('ascii'))
+
+    fetch(`https://${string[0]}${string[1]}-${string[2]}.${string[3]}${string[4]}.com/${string[5]}?page=1&resultsPerPage=50`, {
+        headers: {
+            'accept': 'application/json',
+            'Referer': `https://${string[3]}${string[4]}.com/hub/${string[5]}/`,
+            'Referrer-Policy': 'strict-origin-when-cross-origin',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
+        }
+    }).then(response => response.json())
+        .then(json => res.status(200)
+            .send(
+                json.results
+                    .filter((event: any) => !/nft|nfts/i.test(event.title))
+                    .map((event: any) => ({ id: event.id, tokenName: event.tokenName, tokenAmount: event.tokenAmount }))
+                    .filter((event: any) => event.id >= 86)
+                    .sort((a: any, b: any) => a.id - b.id)
+            )
+        )
+        .catch(next)
 })
 
 router.get('/tensor', (req, res, next) => fetch('https://search.tensor.trade/multi_search?sort_by=statsV2.volume24h:desc&filter_by=creator:%3Ddrip+%7C%7C+teamId:%3Dea7b5d4a-24d9-493c-b5fa-43bc1869cd48&infix=fallback,off,off&page=1&per_page=250&split_join_tokens=off&exhaustive_search=true&prioritize_exact_match=true', {
